6292189e554ea789582bc77276aa41ffea7c2aac
Clyde Limbaga <clydes0218@gmail.com>
2025-10-14 20:59:53 +0800

    Update DatabaseConnection.java

--- All In One/DatabaseConnection.java
+++ All In One/DatabaseConnection.java
@@ -1,10 +1,9 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class DatabaseConnection {
     protected Connection con;
 
-    public void connect() throws Exception {
+    public void sql() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
         con = DriverManager.getConnection(
             "jdbc:mysql://localhost:3307/systemdb",  
