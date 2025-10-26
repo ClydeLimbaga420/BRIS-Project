b989899d35c9a013645a1f57e3c60d5d0700a78f
Clyde Limbaga <clydes0218@gmail.com>
2025-10-26 15:29:33 +0800

    Create BrisApplication.java

---
+++ Backend/com.BRIS.Bris/BrisApplication.java
@@ -0,0 +1,13 @@
+package com.BRIS.Bris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrisApplication.class, args);
+	}
+
+}
