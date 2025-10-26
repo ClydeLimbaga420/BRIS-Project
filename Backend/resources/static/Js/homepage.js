3982b87d8accfb06765d192032e3e77788198111
Clyde Limbaga <clydes0218@gmail.com>
2025-10-26 20:14:43 +0800

    Create homepage.js

---
+++ Backend/resources/static/Js/homepage.js
@@ -0,0 +1,6 @@
+const menuBtn = document.getElementById("menu-btn");
+const sidebar = document.getElementById("sidebar");
+
+menuBtn.addEventListener("click", () => {
+  sidebar.style.display = sidebar.style.display === "block" ? "none" : "block";
+});
