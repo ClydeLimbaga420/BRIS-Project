af98cd6fbcd4de23c93412fbb9b537d3e555f874
Clyde Limbaga <clydes0218@gmail.com>
2025-10-26 15:26:13 +0800

    Create UserRepository.java

---
+++ Backend/com.BRIS.Bris/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.BRIS.Bris.repository;
+
+import com.BRIS.Bris.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsernameAndPassword(String username, String password);
+}
