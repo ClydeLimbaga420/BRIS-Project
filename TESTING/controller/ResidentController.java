011bfecc926f60977625b61a12cc27f1d9a8f93e
Jaymarkpimentel <jaymarkpimentel4@gmail.com>
2025-10-27 18:09:01 +0800

    Update ResidentController.java

--- TESTING/controller/ResidentController.java
+++ TESTING/controller/ResidentController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/residents")
+@RequestMapping("/residents")
 @CrossOrigin(origins = "*") // Allow frontend access
 public class ResidentController {
 
