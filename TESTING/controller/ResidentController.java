943047165627da91eb30fa4c7517352bd50c35f3
Jaymarkpimentel <jaymarkpimentel4@gmail.com>
2025-10-27 18:03:06 +0800

    Create  ResidentController.java

---
+++ TESTING/controller/ResidentController.java
@@ -0,0 +1,26 @@
+package codinpad.controller;//change this
+
+import codinpad.model.Resident;//change this
+import codinpad.service.ResidentService;//change this
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/residents")
+@CrossOrigin(origins = "*") // Allow frontend access
+public class ResidentController {
+
+    private final ResidentService residentService;
+
+    @Autowired
+    public ResidentController(ResidentService residentService) {
+        this.residentService = residentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Resident> addResident(@RequestBody Resident resident) {
+        Resident savedResident = residentService.saveResident(resident);
+        return ResponseEntity.ok(savedResident);
+    }
+}
