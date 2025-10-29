e6b79e89ebf7ef74b8c9bf431787d487eeaf170d
Clyde Limbaga <clydes0218@gmail.com>
2025-10-29 17:56:23 +0800

    Update UserService.java

--- Backend/com.BRIS.Bris/service/UserService.java
+++ Backend/com.BRIS.Bris/service/UserService.java
@@ -4,6 +4,8 @@ import com.BRIS.Login.entity.User;
 import com.BRIS.Login.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +14,23 @@ public class UserService {
     private final UserRepository userRepository;
 
     public User login(String gmailAccount, String password) {
+
+        password = hashPassword(password);
         return userRepository.findByGmailAccountAndPassword(gmailAccount, password);
+    }
+
+   
+    private String hashPassword(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = md.digest(input.getBytes(StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hashBytes) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            throw new RuntimeException("Error hashing password", e);
+        }
     }
 }
