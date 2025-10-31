73327088d1a78c4e9c135e5d3150a8e2afcb5973
Clyde Limbaga <clydes0218@gmail.com>
2025-10-31 14:34:18 +0800

    Update homepage.js

--- Backend/resources/static/Js/homepage.js
+++ Backend/resources/static/Js/homepage.js
@@ -1,6 +1,36 @@
-const menuBtn = document.getElementById("menu-btn");
-const sidebar = document.getElementById("sidebar");
 
-menuBtn.addEventListener("click", () => {
-  sidebar.style.display = sidebar.style.display === "block" ? "none" : "block";
+const Menubar = document.getElementById("dropdown");
+const MenuDropdown = document.getElementById("menu_dropdown");
+
+Menubar.addEventListener("click", () => {
+  if(MenuDropdown.style.display === "none") {
+    MenuDropdown.style.display = "block";
+  } else {
+    MenuDropdown.style.display = "none";
+  }
 });
+
+const logoutbutton = document.getElementById("logoutbutton");
+const logoutconfirm = document.getElementById("logoutconfirm");
+const cancel = document.getElementById("cancel");
+const logout = document.getElementById("logout");
+
+logoutbutton.addEventListener("click", () => {
+logoutconfirm.style.display = "flex";
+});
+
+cancel.addEventListener("click", () => {
+logoutconfirm.style.display = "none";
+});
+
+logout.addEventListener("click", () => {
+window.location.href = "/login";
+});
+
+window.addEventListener("click", (e) => {
+if (e.target === logoutconfirm) {
+logoutconfirm.style.display = "none";
+}
+});
+
+
