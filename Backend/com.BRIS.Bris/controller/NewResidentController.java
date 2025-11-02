520ca3fd0a50abd78dfd6f187a486048ece85045
Clyde Limbaga <clydes0218@gmail.com>
2025-11-02 17:53:50 +0800

    Add files via upload

---
+++ Backend/com.BRIS.Bris/controller/NewResidentController.java
@@ -0,0 +1,52 @@
+package com.BRIS.Login.controller;
+
+import com.BRIS.Login.entity.NewResident;
+import com.BRIS.Login.repository.NewResidentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Controller
+public class NewResidentController {
+
+    @Autowired
+    private NewResidentRepository newResidentRepository;
+
+    @PostMapping("/residents/save")
+    public String saveResident(NewResident newResident,
+                               @RequestParam(value = "photoFile", required = false) MultipartFile file, Model model) {
+        try {
+            if (file != null && !file.isEmpty()) {
+                newResident.setPhoto(file.getBytes());
+            }
+
+
+            if (newResident.getAge() >= 60) {
+                newResident.setSenior(true);
+            } else {
+                newResident.setSenior(false);
+            }
+
+            newResidentRepository.save(newResident);
+            model.addAttribute("successMessage","Resident Added Succesfully!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("errorMessage","Error");
+        }
+
+
+        return "redirect:/residents";
+    }
+
+    @GetMapping("/residents")
+    public String viewResidents(Model model) {
+        model.addAttribute("residents", newResidentRepository.findAll());
+        return "newresident";
+    }
+}
