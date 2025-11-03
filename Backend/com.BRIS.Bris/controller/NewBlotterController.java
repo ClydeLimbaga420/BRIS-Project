b65bbce96b7261cb6b7fdbdd00ea1a4de7230d82
Clyde Limbaga <clydes0218@gmail.com>
2025-11-03 11:41:12 +0800

    Create NewBlotterController.java

---
+++ Backend/com.BRIS.Bris/controller/NewBlotterController.java
@@ -0,0 +1,29 @@
+package com.BRIS.Login.controller;
+
+import com.BRIS.Login.entity.NewBlotter;
+import com.BRIS.Login.repository.NewBlotterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class NewBlotterController {
+
+    @Autowired
+    private NewBlotterRepository blotterRepository;
+
+    @PostMapping("/blotter/add")
+    public String addBlotter(NewBlotter blotter, Model model) {
+        try {
+            blotterRepository.save(blotter);
+            model.addAttribute("successMessage", "Blotter report added successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("errorMessage", "Error saving report: " + e.getMessage());
+        }
+
+        
+        return "reportcase"; 
+    }
+}
