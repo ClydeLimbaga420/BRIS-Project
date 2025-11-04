d5a80783936031147800f5fb4e6eadea72d84fa2
Jaymarkpimentel <jaymarkpimentel4@gmail.com>
2025-11-04 18:54:36 +0800

    Delete TESTING directory

--- TESTING/repository/ResidentRepository.java
+++
@@ -1,7 +0,0 @@
-package codinpad.repository;//change this
-
-import codinpad.model.Resident;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResidentRepository extends JpaRepository<Resident, Long> {
-}
