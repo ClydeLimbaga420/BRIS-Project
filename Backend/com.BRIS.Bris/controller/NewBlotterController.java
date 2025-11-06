6c8dda1187ff11f3546ea2d7b55756d962701305
Clyde Limbaga <clydes0218@gmail.com>
2025-11-06 16:58:16 +0800

    Update NewBlotterController.java

--- Backend/com.BRIS.Bris/controller/NewBlotterController.java
+++ Backend/com.BRIS.Bris/controller/NewBlotterController.java
@@ -1,6 +1,8 @@
 package com.BRIS.Login.controller;
 
+import com.BRIS.Login.entity.History;
 import com.BRIS.Login.entity.NewBlotter;
+import com.BRIS.Login.repository.HistoryRepository;
 import com.BRIS.Login.repository.NewBlotterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,10 +15,16 @@ public class NewBlotterController {
     @Autowired
     private NewBlotterRepository blotterRepository;
 
+    @Autowired
+    private HistoryRepository historyRepository;
+
     @PostMapping("/blotter/add")
     public String addBlotter(NewBlotter blotter, Model model) {
         try {
             blotterRepository.save(blotter);
+
+            History history = new History("Added a new Blotter for: " + blotter.getComfirstname() + " " + blotter.getComlastname() );
+            historyRepository.save(history);
             model.addAttribute("successMessage", "Blotter report added successfully!");
         } catch (Exception e) {
             e.printStackTrace();
