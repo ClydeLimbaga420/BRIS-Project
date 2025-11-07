f9923a1a2c1b227acd934cffef95d62ff7322e0e
Clyde Limbaga <clydes0218@gmail.com>
2025-11-07 16:50:49 +0800

    Add files via upload

--- Backend/com.BRIS.Bris/controller/ResidentsController.java
+++ Backend/com.BRIS.Bris/controller/ResidentsController.java
@@ -1,8 +1,12 @@
 package com.BRIS.Login.controller;
 
 import com.BRIS.Login.entity.Resident;
+import com.BRIS.Login.repository.HistoryRepository;
 import com.BRIS.Login.repository.ResidentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -13,9 +17,10 @@ public class ResidentsController {
     @Autowired
     private ResidentRepository residentRepository;
 
+    private HistoryRepository historyRepository;
+
     @GetMapping
     public List<Resident> getAllResidents() {
-
         return residentRepository.findAll();
     }
 
@@ -33,4 +38,24 @@ public class ResidentsController {
                 })
                 .toList();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Resident> getResidentById(@PathVariable Long id) {
+        return residentRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/{id}/photo")
+    public ResponseEntity<byte[]> getResidentPhoto(@PathVariable Long id) {
+        return residentRepository.findById(id)
+                .filter(resident -> resident.getPhoto() != null)
+                .map(resident -> ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"photo_" + id + ".jpg\"")
+                        .contentType(MediaType.IMAGE_JPEG)
+                        .body(resident.getPhoto()))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
 }
